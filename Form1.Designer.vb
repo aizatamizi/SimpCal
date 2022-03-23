ca6936fabc0cf3d708ef06608387484276773652
ASUS <ASUS@LAPTOP-UDHPNQ86>
2022-03-23 23:23:41 +0800

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,107 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.label1 = New System.Windows.Forms.Label()
+        Me.label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txt1 = New System.Windows.Forms.TextBox()
+        Me.txt2 = New System.Windows.Forms.TextBox()
+        Me.btnSubmit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'label1
+        '
+        Me.label1.AutoSize = True
+        Me.label1.Location = New System.Drawing.Point(120, 58)
+        Me.label1.Name = "label1"
+        Me.label1.Size = New System.Drawing.Size(64, 20)
+        Me.label1.TabIndex = 0
+        Me.label1.Text = "Integer1"
+        '
+        'label2
+        '
+        Me.label2.AutoSize = True
+        Me.label2.Location = New System.Drawing.Point(116, 153)
+        Me.label2.Name = "label2"
+        Me.label2.Size = New System.Drawing.Size(68, 20)
+        Me.label2.TabIndex = 1
+        Me.label2.Text = "Integer 2"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 290)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(53, 20)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Label3"
+        '
+        'txt1
+        '
+        Me.txt1.Location = New System.Drawing.Point(84, 107)
+        Me.txt1.Name = "txt1"
+        Me.txt1.Size = New System.Drawing.Size(125, 27)
+        Me.txt1.TabIndex = 3
+        '
+        'txt2
+        '
+        Me.txt2.Location = New System.Drawing.Point(84, 188)
+        Me.txt2.Name = "txt2"
+        Me.txt2.Size = New System.Drawing.Size(125, 27)
+        Me.txt2.TabIndex = 4
+        '
+        'btnSubmit
+        '
+        Me.btnSubmit.Location = New System.Drawing.Point(99, 237)
+        Me.btnSubmit.Name = "btnSubmit"
+        Me.btnSubmit.Size = New System.Drawing.Size(94, 29)
+        Me.btnSubmit.TabIndex = 5
+        Me.btnSubmit.Text = "Submit"
+        Me.btnSubmit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(314, 423)
+        Me.Controls.Add(Me.btnSubmit)
+        Me.Controls.Add(Me.txt2)
+        Me.Controls.Add(Me.txt1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.label2)
+        Me.Controls.Add(Me.label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents label1 As Label
+    Friend WithEvents label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txt1 As TextBox
+    Friend WithEvents txt2 As TextBox
+    Friend WithEvents btnSubmit As Button
+End Class
